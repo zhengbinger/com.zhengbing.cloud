0ed91492ccb039d98827717e14334c39b2e9b503
AngularZheng <mydreambing@126.com>
2019-07-30 22:33:20 +0800

    simplify module's pom config,put the most public depency to parent

--- register-center/src/main/java/com/zhengbing/cloud/register/RegisterCenterApplication.java
+++ register-center/src/main/java/com/zhengbing/cloud/register/RegisterCenterApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * to see the document of official
+ * can see that,the eureka 2.0 was Discontinued to open source
+ * but they also said "Eureka 1.x is a core part of Netflix's service discovery system and is still an active project."
+ * so we can change the depency to eureka 1.x to resolve the question of eureka 2.0 discontinued to open source
+ * <p></p>
  * register-center core work is add  annotation EnableEurekaServer and configuration application.yml
  */
 @EnableEurekaServer
