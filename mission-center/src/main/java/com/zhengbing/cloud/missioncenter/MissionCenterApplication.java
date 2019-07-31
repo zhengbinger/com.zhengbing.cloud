d3a031a2f2868e0ca5f05022b8775cc544d6c346
zhengbing_vendor <zhengbing_vendor@sensetime.com>
2019-07-31 20:20:21 +0800

    mission  init

--- mission-center/src/main/java/com/zhengbing/cloud/missioncenter/MissionCenterApplication.java
+++ mission-center/src/main/java/com/zhengbing/cloud/missioncenter/MissionCenterApplication.java
@@ -1,18 +1,16 @@
-package com.zhengbing.cloud.gatewaycenter;
+package com.zhengbing.cloud.missioncenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 
-@EnableZuulServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayCenterApplication {
+public class MissionCenterApplication {
 
     public static void main ( String[] args ) {
-        SpringApplication.run( GatewayCenterApplication.class, args );
+        SpringApplication.run( MissionCenterApplication.class, args );
     }
 
 }
