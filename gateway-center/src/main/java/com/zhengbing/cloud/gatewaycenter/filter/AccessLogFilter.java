a7f7d248419642bbb3244d630027a831d24003a6
zhengbing <mydreambing@126.com>
2020-03-22 00:24:59 +0800

    authorization-center  finished  add comment

---
+++ gateway-center/src/main/java/com/zhengbing/cloud/gatewaycenter/filter/AccessLogFilter.java
@@ -0,0 +1,48 @@
+package com.zhengbing.cloud.gatewaycenter.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author zhengbing
+ * @descript
+ * @date 2019-08-17
+ * @email mydreambing@126.com
+ * @since version 1.0
+ */
+
+@Slf4j
+@Component
+public class AccessLogFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        // 需要最后一个执行的filter
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        log.info("Request \"{}\" spent : {} seconds.", request.getRequestURI(),
+                (System.currentTimeMillis() - Long.valueOf(requestContext.get("api_request_time").toString())) / 1000);
+        return null;
+    }
+}
---
+++ gateway-center/src/main/java/com/zhengbing/cloud/gatewaycenter/filter/PreFilter.java
@@ -0,0 +1,45 @@
+package com.zhengbing.cloud.gatewaycenter.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhengbing
+ * @descript
+ * @date 2019-08-17
+ * @email mydreambing@126.com
+ * @since version 1.0
+ */
+
+@Slf4j
+@Component
+public class PreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //获取当前请求的请求上下文
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        //记录请求进入时间
+        requestContext.set("api_request_time", System.currentTimeMillis());
+        return null;
+    }
+}
