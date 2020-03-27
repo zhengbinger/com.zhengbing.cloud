4ac9fdc49dbe96d9ed4a7a125ab222a71babd44f
binger-zheng <Zb@123456>
2020-03-27 18:12:10 +0800

    添加springboot-admin  服务监控管理

--- server-monitor/src/main/java/com/zhengbing/cloud/servermonitor/ServerMonitorApplication.java
+++ server-monitor/src/main/java/com/zhengbing/cloud/servermonitor/ServerMonitorApplication.java
@@ -3,8 +3,10 @@ package com.zhengbing.cloud.servermonitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServerMonitorApplication {
 
