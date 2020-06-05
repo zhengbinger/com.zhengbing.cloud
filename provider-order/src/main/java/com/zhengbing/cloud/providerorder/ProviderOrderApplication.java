4c8b6a65d108e4a9194b73d64699033bd44d85e3
zhengbing_vendor <Zb@123456>
2020-06-05 21:12:52 +0800

    添加订单服务

---
+++ provider-order/src/main/java/com/zhengbing/cloud/providerorder/ProviderOrderApplication.java
@@ -0,0 +1,13 @@
+package com.zhengbing.cloud.providerorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderOrderApplication.class, args);
+    }
+
+}
---
+++ provider-order/src/test/java/com/zhengbing/cloud/providerorder/ProviderOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengbing.cloud.providerorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
